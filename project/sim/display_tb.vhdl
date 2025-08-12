f2f8449a40f0c41082d6a841a3fb0a882d8935f6
davidsanchezreal1991 <davidsanchezreal1991@gmail.com>
2025-08-12 02:36:07 +0200

    Pines
    -Se añaden las constrains y los pines de toda la PCB shield.
    -Se quita la señal de enable, no me parece necesaria.

--- project/sim/display_tb.vhdl
+++ project/sim/display_tb.vhdl
@@ -8,22 +8,20 @@ end entity;
 
 architecture test of display_tb is
   signal s_clk      : std_logic := '0';
-  signal s_rst      : std_logic := '0';
-  signal s_en       : std_logic := '0';
+  signal s_rst_n    : std_logic := '0';
   signal s_input1   : std_logic_vector(4 downto 0) := "00000";
   signal s_input2   : std_logic_vector(4 downto 0) := "00000";
   signal s_input3   : std_logic_vector(4 downto 0) := "00000";
   signal s_seg      : std_logic_vector(2 downto 0) := "000";
   signal s_led      : std_logic_vector(7 downto 0) := "00000000";
-  constant T_CLK    : time := 5 ns;
-  constant T_DLY    : time := 20 ns;
+  constant T_CLK    : time := 20 ns;
+  constant T_DLY    : time := 100 ns;
   begin
   dut : entity work.display
     port map
     (
       clk     => s_clk,
-      rst     => s_rst,
-      en      => s_en,
+      rst_n   => s_rst_n,
       input1  => s_input1,
       input2  => s_input2,
       input3  => s_input3
@@ -31,15 +29,12 @@ architecture test of display_tb is
   s_clk <= not s_clk after T_CLK;
   process 
     begin
-      s_rst <= '1';
-      s_en  <= '0';
+      s_rst_n  <= '0';
       s_input1 <= "00000";
       s_input2 <= "00000";
       s_input3 <= "00000";
       wait for T_DLY;
-      s_rst <= '0';
-      wait for T_DLY;
-      s_en  <= '1';
+      s_rst_n  <= '1';
       wait for T_DLY;
       for i in 0 to 15 loop
         s_input1 <= std_logic_vector(to_unsigned(i, 5));
@@ -53,8 +48,7 @@ architecture test of display_tb is
         s_input3 <= std_logic_vector(to_unsigned(i, 5));
         wait for T_DLY;
       end loop;
-      s_rst <= '1';
-      s_en  <= '0';
+      s_rst_n <= '0';
       wait for 2*T_DLY;
 
       assert false
--- project/src/display.vhdl
+++ project/src/display.vhdl
@@ -5,100 +5,111 @@ use ieee.numeric_std.all;
 entity display is
   port  (
         clk     :   in    std_logic;
-        rst     :   in    std_logic;
-        en      :   in    std_logic;
+        rst_n   :   in    std_logic;
         input1  :   in    std_logic_vector(4 downto 0);
         input2  :   in    std_logic_vector(4 downto 0);
         input3  :   in    std_logic_vector(4 downto 0);
-        seg     :   out   std_logic_vector(2 downto 0);
+        input4  :   in    std_logic_vector(4 downto 0);
+        input5  :   in    std_logic_vector(4 downto 0);
+        input6  :   in    std_logic_vector(4 downto 0);
+        input7  :   in    std_logic_vector(4 downto 0);
+        input8  :   in    std_logic_vector(4 downto 0);
+        seg     :   out   std_logic_vector(7 downto 0);
         led     :   out   std_logic_vector(7 downto 0)
         );
 end entity;
 
 architecture rtl of display is
 
-  signal  contador  :   std_logic_vector(1 downto 0) := (others => '0');
+  signal contador   :   unsigned(2 downto 0)         := (others => '0');
   signal  s_input   :   std_logic_vector(4 downto 0) := (others => '0');
   begin
 
-  segs: process (clk, rst)
+  segs: process (clk, rst_n)
     begin
-      if rst = '1' then
+      if rst_n = '0' then
         contador  <= (others => '0');
         seg       <= (others => '0');
+        s_input   <= (others => '0');
       elsif rising_edge(clk) then
-        if en = '1' then
-          case contador is
-            when "00" =>
-              seg       <= "000";
-              s_input    <= (others => '0');
-              contador  <= "01";
-            when "01" =>
-              seg       <= "001";
-              s_input    <= input1;
-              contador  <= "10";
-            when "10" =>
-              seg       <= "010";
-              s_input    <= input2;
-              contador  <= "11";
-            when "11" =>
-              seg       <= "100";
-              s_input    <= input3;
-              contador  <= "01";
-            when others   =>
-              seg  <= (others => 'X');
-            end case;
-        else
-          seg       <= (others => '0');
-          contador  <= (others => '0');
-        end if;
+        case contador is
+          when 0 =>
+            seg       <= "00000001";
+            s_input   <= input1;
+            contador  <= 1;
+          when 1 =>
+            seg       <= "00000010";
+            s_input   <= input2;
+            contador  <= 2;
+          when 2 =>
+            seg       <= "00000100";
+            s_input   <= input3;
+            contador  <= 3;
+          when 3 =>
+            seg       <= "00001000";
+            s_input   <= input4;
+            contador  <= 4;
+          when 4 =>
+            seg       <= "00010000";
+            s_input   <= input5;
+            contador  <= 5;
+          when 5 =>
+            seg       <= "00100000";
+            s_input   <= input6;
+            contador  <= 6;
+          when 6 =>
+            seg       <= "01000000";
+            s_input   <= input7;
+            contador  <= 7;
+          when 7 =>
+            seg       <= "10000000";
+            s_input   <= input8;
+            contador  <= 8;
+          when others => seg  <= (others => 'X');
+        end case;
       end if;
   end process segs;
 
-  leds: process (clk, rst)
+  leds: process (clk, rst_n)
     begin
-      if rst = '1' then
+      if rst_n = '0' then
         led <= (others => '1');
       elsif rising_edge(clk) then
-        if en = '1' then
-          case s_input is
-            when "00000" =>  led <= "00000011";
-            when "00001" =>  led <= "10011111";
-            when "00010" =>  led <= "00100101";
-            when "00011" =>  led <= "00001101";
-            when "00100" =>  led <= "10011001";
-            when "00101" =>  led <= "01001001";
-            when "00110" =>  led <= "01000001";
-            when "00111" =>  led <= "00011111";
-            when "01000" =>  led <= "00000001";
-            when "01001" =>  led <= "00001001";
-            when "01010" =>  led <= "00010001";
-            when "01011" =>  led <= "11000001";
-            when "01100" =>  led <= "01100011";
-            when "01101" =>  led <= "10000101";
-            when "01110" =>  led <= "01100001";
-            when "01111" =>  led <= "01110001";
-            when "10000" =>  led <= "00000010";
-            when "10001" =>  led <= "10011110";
-            when "10010" =>  led <= "00100100";
-            when "10011" =>  led <= "00001100";
-            when "10100" =>  led <= "10011000";
-            when "10101" =>  led <= "01001000";
-            when "10110" =>  led <= "01000000";
-            when "10111" =>  led <= "00011110";
-            when "11000" =>  led <= "00000000";
-            when "11001" =>  led <= "00001000";
-            when "11010" =>  led <= "00010000";
-            when "11011" =>  led <= "11000000";
-            when "11100" =>  led <= "01100010";
-            when "11101" =>  led <= "10000100";
-            when "11110" =>  led <= "01100000";
-            when "11111" =>  led <= "01110000";
-            when others  =>  led <= (others => 'X');
-          end case;
-        else
-          led <= (others => '1');
-        end if;
+        case s_input is
+          when "00000" =>  led <= "00000011";
+          when "00001" =>  led <= "10011111";
+          when "00010" =>  led <= "00100101";
+          when "00011" =>  led <= "00001101";
+          when "00100" =>  led <= "10011001";
+          when "00101" =>  led <= "01001001";
+          when "00110" =>  led <= "01000001";
+          when "00111" =>  led <= "00011111";
+          when "01000" =>  led <= "00000001";
+          when "01001" =>  led <= "00001001";
+          when "01010" =>  led <= "00010001";
+          when "01011" =>  led <= "11000001";
+          when "01100" =>  led <= "01100011";
+          when "01101" =>  led <= "10000101";
+          when "01110" =>  led <= "01100001";
+          when "01111" =>  led <= "01110001";
+          when "10000" =>  led <= "00000010";
+          when "10001" =>  led <= "10011110";
+          when "10010" =>  led <= "00100100";
+          when "10011" =>  led <= "00001100";
+          when "10100" =>  led <= "10011000";
+          when "10101" =>  led <= "01001000";
+          when "10110" =>  led <= "01000000";
+          when "10111" =>  led <= "00011110";
+          when "11000" =>  led <= "00000000";
+          when "11001" =>  led <= "00001000";
+          when "11010" =>  led <= "00010000";
+          when "11011" =>  led <= "11000000";
+          when "11100" =>  led <= "01100010";
+          when "11101" =>  led <= "10000100";
+          when "11110" =>  led <= "01100000";
+          when "11111" =>  led <= "01110000";
+          when others  =>  led <= (others => 'X');
+        end case;
       end if;
     end process leds;
 
